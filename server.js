69207690557fb2d55f105152eb1c5f1781f791f7
Julia <julia.gray30@gmail.com>
2022-03-30 22:10:58 +0100

    Getting an error - Router requires middleware function...

--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
-const routes = require("./routes");
+const exphbs = require("express-handlebars")
+const path = require("path")
+const routes = require("./controllers/");
+const hbs = exphbs.create({})
+
 const sequelize = require("./config/connection");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.engine("handlebars", hbs.engine)
+app.set("view engine", "handlebars")
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")))
 
 app.use(routes);
 
